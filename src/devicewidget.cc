dea5d0a0fe353cd2a23bb379605f09011ce240d8
Colin Guthrie <cguthrie@mandriva.org>
2009-03-18 21:07:49 +0000

    Include config.h in the cc file (after rebase)

--- src/devicewidget.cc
+++ src/devicewidget.cc
@@ -18,6 +18,10 @@
   along with pavucontrol. If not, see <http://www.gnu.org/licenses/>.
 ***/
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "devicewidget.h"
 #include "channelwidget.h"
 
