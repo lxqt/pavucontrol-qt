1f037dc341cce2f47c8b2806c439d5503cf9624c
Tanu Kaskinen <tanuk@iki.fi>
2013-03-11 13:16:48 +0200

    build-sys: Use autoreconf in bootstrap.sh
    Most of the stuff in bootstrap.sh seemed to reimplement autoreconf,
    badly. I don't understand the wisdom behind the run_versioned
    trickery. configure.ac states that we require Automake 1.11, so
    apparently the VERSION variable in bootstrap.sh was out of date.
    Instead of bumping the version number, I replaced the cruft with
    an autoreconf call.

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,57 +17,16 @@
 # You should have received a copy of the GNU General Public License
 # along with pavucontrol. If not, see <http://www.gnu.org/licenses/>.
 
-VERSION=1.9
-
-run_versioned() {
-    local P
-    local V
-
-    V=$(echo "$2" | sed -e 's,\.,,g')
-
-    if [ -e "`which $1$V 2> /dev/null`" ] ; then
-        P="$1$V"
-    else
-        if [ -e "`which $1-$2 2> /dev/null`" ] ; then
-            P="$1-$2"
-        else
-            P="$1"
-        fi
-    fi
-
-    shift 2
-    "$P" "$@"
-}
-
-set -ex
-
 if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
     cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
     chmod +x .git/hooks/pre-commit && \
     echo "Activated pre-commit hook."
 fi
 
-if [ "x$1" = "xam" ] ; then
-    run_versioned automake "$VERSION" -a -c --foreign
-    ./config.status
-else
-    rm -rf autom4te.cache
-    rm -f config.cache
+intltoolize --force --automake
+autoreconf --force --install --symlink
 
-    rm -f Makefile.am~ configure.ac~
-    # Evil, evil, evil, evil hack
-    sed 's/read dummy/\#/' `which gettextize` | sh -s -- --copy --force
-    test -f Makefile.am~ && mv Makefile.am~ Makefile.am
-    test -f configure.ac~ && mv configure.ac~ configure.ac
-
-    intltoolize --copy --force --automake
-    run_versioned aclocal "$VERSION" -I m4
-    run_versioned autoconf 2.59 -Wall
-    run_versioned autoheader 2.59
-    run_versioned automake "$VERSION" -a -c --foreign
-
-    if test "x$NOCONFIGURE" = "x"; then
-        CFLAGS="-g -O0" ./configure --sysconfdir=/etc --localstatedir=/var "$@"
-        make clean
-    fi
+if test "x$NOCONFIGURE" = "x"; then
+    CFLAGS="-g -O0" ./configure --sysconfdir=/etc --localstatedir=/var "$@"
+    make clean
 fi
