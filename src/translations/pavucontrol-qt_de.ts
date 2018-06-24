fae7513da58d33e754ab736abefcb457097e4d10
p8q <w45jr6y4ih@bzegi.anonbox.net>
2018-06-24 21:14:03 +0000

    Translated using Weblate (German)
    Currently translated at 90.2% (121 of 134 strings)
    
    Translation: LXQt/PAVUControl-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/pavucontrol-qt/de/

--- src/translations/pavucontrol-qt_de.ts
+++ src/translations/pavucontrol-qt_de.ts
@@ -722,7 +722,7 @@ Wenn dies der Fall ist, sollte PulseAudio wieder automatisch starten. Wenn autom
     <message>
         <location filename="../streamwidget.ui" line="66"/>
         <source>Mute audio</source>
-        <translation type="unfinished"></translation>
+        <translation>Audio stumm schalten</translation>
     </message>
     <message>
         <location filename="../streamwidget.ui" line="79"/>
