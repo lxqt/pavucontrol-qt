5cf2658824b402b12b753d0f926b8519d0dfe682
p8q <w45jr6y4ih@bzegi.anonbox.net>
2018-06-24 21:10:42 +0000

    Translated using Weblate (German)
    Currently translated at 87.3% (117 of 134 strings)
    
    Translation: LXQt/PAVUControl-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/pavucontrol-qt/de/

--- src/translations/pavucontrol-qt_de.ts
+++ src/translations/pavucontrol-qt_de.ts
@@ -549,7 +549,7 @@ Wenn dies der Fall ist, sollte PulseAudio wieder automatisch starten. Wenn autom
     <message>
         <location filename="../pavucontrol.cc" line="675"/>
         <source>Retry forever if pa quits (every 5 seconds).</source>
-        <translation type="unfinished"></translation>
+        <translation>Wiederholen für immer wenn pa sich beendet (alle 5 Sekunden). </translation>
     </message>
     <message>
         <location filename="../pavucontrol.cc" line="678"/>
