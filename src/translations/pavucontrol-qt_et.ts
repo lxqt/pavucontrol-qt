c2a7e03ff4ca5c89d70f6cf4664082f0b3e00d3b
Weblate <noreply@weblate.org>
2026-09-23 18:29:44 +0200

    weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/pavucontrol-qt/et/
    Translation: LXQt Configuration/pavucontrol-qt

--- src/translations/pavucontrol-qt_et.ts
+++ src/translations/pavucontrol-qt_et.ts
@@ -102,7 +102,7 @@
     <message>
         <location filename="../devicewidget.ui" line="85"/>
         <source>Set as default</source>
-        <translation type="unfinished"></translation>
+        <translation>Määra vaikimisi väärtuseks</translation>
     </message>
     <message>
         <location filename="../devicewidget.ui" line="103"/>
